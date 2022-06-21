d721b8a240f68d7c95ab1cf2a4ba4af3425d12a4
Jeremy Evans <code@jeremyevans.net>
2022-06-21 13:06:47 -0700

    Work with rack-test 2.0.0+
    rack-test 2.0.0+ hasn't been released yet, but this change will
    allow capybara-restore_state to work with it.

--- lib/capybara/restore_state.rb
+++ lib/capybara/restore_state.rb
@@ -3,7 +3,8 @@ require 'capybara'
 module Capybara::RestoreState
   # Yield to the block, restoring the capybara session state after block execution.
   def restore_state
-    mock_session = page.driver.browser.instance_variable_get(:@_rack_mock_sessions)[:default]
+    browser = page.driver.browser
+    mock_session = browser.instance_variable_get(:@_rack_test_current_session) || browser.instance_variable_get(:@_rack_mock_sessions)[:default]
     last_response = mock_session.last_response
     last_request = mock_session.last_request
     yield
